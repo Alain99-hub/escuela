69801cdaa124936c2f0b0cb05269799a2f0ed566
Alain99-hub <theforestdeath99@gmail.com>
2020-09-09 20:39:16 -0500

    Segunda parte

--- src/learning/java/school/Application.java
+++ src/learning/java/school/Application.java
@@ -14,20 +14,31 @@ public class Application {
         };
 
         Scanner scanner = new Scanner(System.in);
-
+        int []aux=new int[Student.length];
         for(int i = 0; i <Student.length; i++){
             System.out.println(" ");
             System.out.println("Name: " + Student[i].getName() + " code: " + Student[i].getCode());
             System.out.print("Asistio: ");
             String response = scanner.next();
             if (response.equalsIgnoreCase("Y")) {
+                aux[i]=1;
                 System.out.println(Student[i].getName()+" SI");
             } else if (response.equalsIgnoreCase("N")) {
+                aux[i]=0;
                 System.out.println(Student[i].getName()+ " NO");
             } else {
-                System.out.println("Incorrecto");
+                System.out.println("Intente de nuevo");
                 i--;
             }
+        }
+        System.out.println("-");
+            for (int j=0;j< Student.length;j++){
+                if(aux[j]==1){
+                    System.out.println(Student[j].getName() + ": si Asistio");
+                }
+                else{
+                    System.out.println(Student[j].getName() + ": No Asistio");
+                }
         }
 
     }
